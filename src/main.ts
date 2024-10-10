53d35f8825a2f82c86fc15bdafc0c87713d1e275
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-10-10 16:55:19 -0300

    insert use cliente tag

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+'use client';
 import './style.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.tsx';
 import { setupCounter } from './counter.ts';
 
-function Home() {
+export default function Home() {
   return(
     <App/>
   );
