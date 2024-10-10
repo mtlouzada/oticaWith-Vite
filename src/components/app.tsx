e8baf3c4642a62f2388f81267c9a9be077f63acd
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-10-09 23:47:50 -0300

    css clean

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div id="app">
+    <div>
       <h1>M2 React com Vite + TypeScript</h1>
-      <button id="counter">Clique aqui</button>
+      <button>Clique aqui</button>
     </div>
   );
 }
--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './components/app.tsx';
 import { setupCounter } from './counter.ts';
 
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-// function App() {
-//   return(
-//     <div class="bg-black">
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>M2 React com Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-//   );
-// }
-
-// export default App;
+function Home() {
+  return(
+    <App/>
+  );
+}
