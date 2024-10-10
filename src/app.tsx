272dd22be81eaeeadc1b1121a20a41d150da39d9
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-10-10 19:50:49 -0300

    run dev bug fixed

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import './style.css';
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <main>
+          
+        <p className='text-black size-5'>teste</p>
+         
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+import React from 'react';
+import App from './app';
+
+
+export default function Home() {
+  return(
+    <>
+    <App/>
+    </>
+  );
+}
