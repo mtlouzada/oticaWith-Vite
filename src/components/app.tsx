c488c0bb2b2216bb6f5c360f8a446d271b739e3c
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-10-09 23:42:51 -0300

    try fix error 404

---
+++ src/components/app.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from 'react';
+
+function App() {
+  return (
+    <div id="app">
+      <h1>M2 React com Vite + TypeScript</h1>
+      <button id="counter">Clique aqui</button>
+    </div>
+  );
+}
+
+export default App;
